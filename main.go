4ca0c45992ad8f70f4b06f2b636852654d5357a0
mtulun <taylan.ulun@outlook.com>
2022-02-24 11:54:15 +0300

    Received data fixed

--- main.go
+++ main.go
@@ -15,85 +15,14 @@ import (
 )
 
 func main() {
-	//HOST üzerinde çalıştırıldığında
-	kubeconfig := flag.String("kubeconfig", "kube_config_path", "kubeconfig file path")
 
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		fmt.Printf("\n[ERROR], %s clientcmd config flag error! :\n ", err.Error())
+	const sendermail string = ""
+	const receivermail string = ""
+	var senderpass string = ""
 
-		//POD olarak çalıştırılacağı zaman
-		config, err = rest.InClusterConfig()
-		if err != nil {
-			fmt.Printf("\n[ERROR], %s InClusterConfig error! :\n ", err.Error())
-		}
-	}
+	xdata := getdataobject()
 
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		fmt.Printf("\n[ERROR], %s clientset error! :\n ", err.Error())
-	}
-	//vars
-	ctx := context.Background()
-	opts := metav1.ListOptions{}
-	var sendermail string
-	const receivermail string = "mail_adresi"
-	var senderpass string
-
-	namespacelist, err := clientset.CoreV1().Namespaces().List(ctx, opts)
-	if err != nil {
-		fmt.Printf("\n[ERROR], %s namespace listing error! : ", err.Error())
-	}
-	if len(namespacelist.Items) == 0 {
-		fmt.Printf("\n[ERROR], %s namespace not found! :\n ", err.Error())
-	} else {
-		// fmt.Println("\nNamespace / Pod List")
-		for _, ns := range namespacelist.Items {
-			fmt.Println(ns.Name)
-			podlist, err := clientset.CoreV1().Pods(ns.Name).List(ctx, opts)
-			if err != nil {
-				fmt.Printf("\n[ERROR], %s Pod listing error! :\n ", err.Error())
-			}
-			for _, po := range podlist.Items {
-				podCreationTime := po.GetCreationTimestamp()
-				age := time.Since(podCreationTime.Time).Round(time.Minute)
-				podStatus := po.Status
-
-				var containerRestarts int32
-				var containerReady int
-				var totalContainers int
-
-				for container := range po.Spec.Containers {
-					containerRestarts += podStatus.ContainerStatuses[container].RestartCount
-					if podStatus.ContainerStatuses[container].Ready {
-						containerReady++
-					}
-					totalContainers++
-				}
-
-				name := po.GetName()
-				ready := fmt.Sprintf("%v/%v", containerReady, totalContainers)
-				status := fmt.Sprintf("%v", podStatus.Phase)
-				restarts := fmt.Sprintf("%v", containerRestarts)
-				ageS := age.String()
-
-				data := append([]string{name, ready, status, restarts, ageS})
-				fmt.Println(data)
-
-				if len(restarts) > 0 {
-					fmt.Println("\nPlease, Enter your email credentials")
-					fmt.Println("\nEmail Address: ")
-					fmt.Scanln(&sendermail)
-					fmt.Println("\nEmail Password: ")
-					fmt.Scanln(&senderpass)
-					email(sendermail, receivermail, senderpass, data)
-				} else {
-					email(sendermail, receivermail, senderpass, data)
-				}
-			}
-		}
-	}
-
+	email(sendermail, receivermail, senderpass, xdata)
 }
 
 func email(smail string, rmail string, spwd string, dt []string) {
@@ -125,5 +54,73 @@ func email(smail string, rmail string, spwd string, dt []string) {
 	} else {
 		fmt.Println("Email sent...")
 	}
+}
 
+func getdataobject() []string {
+
+	//HOST üzerinde çalıştırıldığında
+	kubeconfig := flag.String("kubeconfig", "", "kubeconfig file path")
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		fmt.Printf("\n[ERROR], %s clientcmd config flag error! :\n ", err.Error())
+
+		//POD olarak çalıştırılacağı zaman
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			fmt.Printf("\n[ERROR], %s InClusterConfig error! :\n ", err.Error())
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Printf("\n[ERROR], %s clientset error! :\n ", err.Error())
+	}
+
+	//vars
+	ctx := context.Background()
+	opts := metav1.ListOptions{}
+	var data []string
+
+	namespacelist, err := clientset.CoreV1().Namespaces().List(ctx, opts)
+	if err != nil {
+		fmt.Printf("\n[ERROR], %s namespace listing error! : ", err.Error())
+	}
+	if len(namespacelist.Items) == 0 {
+		fmt.Printf("\n[ERROR], %s namespace not found! :\n ", err.Error())
+	} else {
+		for _, ns := range namespacelist.Items {
+			podlist, err := clientset.CoreV1().Pods(ns.Name).List(ctx, opts)
+			if err != nil {
+				fmt.Printf("\n[ERROR], %s Pod listing error! :\n ", err.Error())
+			}
+			for _, po := range podlist.Items {
+				podCreationTime := po.GetCreationTimestamp()
+				age := time.Since(podCreationTime.Time).Round(time.Minute)
+				podStatus := po.Status
+
+				var containerRestarts int32
+				var containerReady int
+				var totalContainers int
+
+				for container := range po.Spec.Containers {
+					containerRestarts += podStatus.ContainerStatuses[container].RestartCount
+					if podStatus.ContainerStatuses[container].Ready {
+						containerReady++
+					}
+					totalContainers++
+				}
+
+				name := po.GetName()
+				ready := fmt.Sprintf("%v/%v", containerReady, totalContainers)
+				status := fmt.Sprintf("%v", podStatus.Phase)
+				restarts := fmt.Sprintf("%v", containerRestarts)
+				ageS := age.String()
+				namespaceName := po.ObjectMeta.Namespace
+
+				data = append(data, []string{namespaceName, name, ready, status, restarts, ageS, "\n"}...)
+			}
+		}
+	}
+	return *&data
 }
